d98fb94d926e096df1fa0b28f7d20bf7c62d2c77
Peter Gault <gault.peter@gmail.com>
2026-01-17 17:50:07 +0000

    Add aggressive caching headers for assets

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import { createMainPageHtml } from './templates/mainPage.js';
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-    
+
     // Serve static assets (images) from the /public folder
     if (url.pathname.startsWith('/cheat-sheet/')) {
-      return env.ASSETS.fetch(request);
+      const response = await env.ASSETS.fetch(request);
+      const newResponse = new Response(response.body, response);
+
+      // Cache for 1 year (immutable) as we want aggressive caching for these static assets
+      newResponse.headers.set('Cache-Control', 'public, max-age=31536000, immutable');
+      return newResponse;
     }
 
     // Handle POST /requests (feature request submissions)
